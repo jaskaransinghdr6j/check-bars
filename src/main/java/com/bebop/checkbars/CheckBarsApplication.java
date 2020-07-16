a4f590cdab6627ad775d36e2731a2455800e1f42
Jaskaran Singh <jaskaran.singh.dr6j@gmail.com>
2020-07-16 15:34:29 -0700

    First Commit

---
+++ src/main/java/com/bebop/checkbars/CheckBarsApplication.java
@@ -0,0 +1,13 @@
+package com.bebop.checkbars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckBarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckBarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bebop/checkbars/CheckBarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebop.checkbars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckBarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
